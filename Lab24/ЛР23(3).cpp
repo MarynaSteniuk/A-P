c14593bed7ba7214ea79254732719026b5ab34ed
MarynaSteniuk <stenyukmarina1@gmail.com>
2025-02-26 20:03:30 +0200

    Update ЛР23(3).cpp
    Corrected errors

--- Lab24/ЛР23(3).cpp
+++ Lab24/ЛР23(3).cpp
@@ -1,32 +1,48 @@
 #include <iostream>
 
-using namespace std;
-
-int main() 
-{
-	setlocale (LC_CTYPE,"ukr");
 float third(float* a, float* b, float* c, float* d) {
-if (*a == *b|| *a == *с || *a == *d || *b==*c || *b==*d || *c == *d) {
-cout<< "вхідні числа не є унікальними"<<endl;
+  if (*a == *b || *a==*c || *a == *d || *b==*c || *b == *d || *c == *d) {
+    std::cout << "the data is not unique" << std::endl;
+  }
+
+  float average = (*a + *b + *c + *d) / 4;
+  std::cout << "average: " << average << std::endl;
+
+  float* minimum = a;
+  float* maximum = a;
+
+  if (*b < *minimum) minimum = b;
+  if (*c < *minimum) minimum = c;
+  if (*d < *minimum) minimum = d;
+
+  if (*b > *maximum) maximum = b;
+  if (*c > *maximum) maximum = c;
+  if (*d > *maximum) maximum = d;
+
+  std::cout << "not minimum and not maximum: ";
+  if (a != minimum && a != maximum) 
+    std::cout << *a << " ";
+  if (b != minimum && b != maximum) 
+    std::cout << *b << " ";
+  if (c != minimum && c != maximum) 
+    std::cout << *c << " ";
+  if (d != minimum && d != maximum) 
+    std::cout << *d << " ";
+
+  return 0;
 }
-float average = (*a + *b + *c + *d) / 4;
-cout<<"Середнє значення: "<<average<<endl;
-float* minNumber = a;
-float* maxNumber = a;
-if (*b < *minNumber) minNumber = b;
-if (*c < *minNumber) minNumber = c;
-if (*d < *minNumber) minNumber = d;
-if (*b > *maxNumber) maxNumber = b;
-if (*c > *maxNumber) maxNumber = c;
-if (*d > *maxNumber) maxNumber = d;
-cout << "два числа != мінімальні і максимальні: ";
-if (a != minNumber && a != maxNumber)
-cout<< *a<< " ";
-if (b != minNumber && b != maxNumber)
-cout<<*b<<" ";
-if (c != minNumber && c != maxNumber)
-cout<<*c<<" ";
-if (d != minNumber && d != maxNumber)
-cout << *d<<" ";
-return 0;
+
+int main() {
+  float a, b, c, d;
+  std::cout << " enter a: ";
+  std::cin >> a;
+  std::cout << " enter b: ";
+  std::cin >> b;
+  std::cout << " enter c: ";
+  std::cin >> c;
+  std::cout << " enter d: ";
+  std::cin >> d;
+  third(&a,&b,&c,&d);
+
+  return 0;
 }
